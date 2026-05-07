90143339159387155ad3103b5725d6be6081bb0d
Guilherme <guifac2006@gmail.com>
2026-05-06 21:49:42 -0300

    Botão JS finalizado(esperar bd)

--- forms.js
+++ forms.js
@@ -23,3 +23,54 @@ btnNovaTransacao.addEventListener('click', function() {
 btnFecharModal.addEventListener('click', function() {
     modalOverlay.style.display = 'none'; 
 });
+
+
+btnDespesa.addEventListener('click', function() {
+    btnDespesa.classList.add('ativo');    
+    btnReceita.classList.remove('ativo'); 
+    tipoAtual = 'despesa';
+});
+
+btnReceita.addEventListener('click', function() {
+    btnReceita.classList.add('ativo');    
+    btnDespesa.classList.remove('ativo'); 
+    tipoAtual = 'receita';
+});
+
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+
+    if (inputDescricao.value.trim() === "" || inputValor.value.trim() === "" || inputData.value === "") {
+        alert("Atenção: Por favor, preencha todos os campos obrigatórios da transação!");
+        return;
+    }
+
+
+    const transaction = {
+        description: inputDescricao.value,
+        amount: Number(inputValor.value),
+        date: inputData.value,
+        category: inputCategoria.value,
+        recurring: inputRecorrente.checked, 
+        type: tipoAtual 
+    };
+
+    
+    const currentData = JSON.parse(localStorage.getItem('transactions')) || [];
+    currentData.push(transaction);
+    localStorage.setItem('transactions', JSON.stringify(currentData));
+
+    console.log("Sucesso! Transação enviada:", transaction);
+
+    
+    form.reset();
+    btnDespesa.classList.add('ativo');
+    btnReceita.classList.remove('ativo');
+    tipoAtual = 'despesa';
+    
+   
+    modalOverlay.style.display = 'none';
+    alert("Transação salva com sucesso!");
+});
