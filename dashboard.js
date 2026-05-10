248644fbc708f2556158a3ab8709e9c5ec933997
Lucas Simões <lucas.simoes622@gmail.com>
2026-05-10 20:22:11 -0300

    feat: adiciona array de transações mock

--- dashboard.js
+++ dashboard.js
@@ -1,3 +1,16 @@
+// ===================================================
 // dashboard.js — Aluno 3: Renderização do DOM
+// ===================================================
 // Esse arquivo lê os dados do data.js e exibe na tela.
+// Ele nunca salva dados, só lê e desenha.
+// ===================================================
 
+// ===================================================
+// MOCK TEMPORÁRIO — apagar quando o Aluno 2 terminar
+// ===================================================
+
+let transacoes = [
+  { id: 1, descricao: "Salário",  valor: 3000, tipo: "receita", categoria: "outros",      data: "2026-05-01" },
+  { id: 2, descricao: "Aluguel",  valor: 1200, tipo: "despesa", categoria: "moradia",     data: "2026-05-05" },
+  { id: 3, descricao: "Mercado",  valor: 350,  tipo: "despesa", categoria: "alimentacao", data: "2026-05-07" }
+];
